969feffa47be6e3866abe63ce7f2ac3e09f94def
Meng Bo <mengbo@lnu.edu.cn>
2017-02-14 14:42:03 +0800

    change style.

--- Casks/wch-ch34x-usb-serial-driver.rb
+++ Casks/wch-ch34x-usb-serial-driver.rb
@@ -5,8 +5,8 @@ cask 'wch-ch34x-usb-serial-driver' do
   url 'https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver/raw/master/CH34x_Install_V1.3.pkg'
   name 'WCH USB serial driver for CH340/CH341 (unofficial release)'
   homepage 'https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver'
-  license :gratis
 
+  license :gratis
 
   pkg 'CH34x_Install_V1.3.pkg'
 
