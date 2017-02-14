81f39d72432f5a82937378b74243e8017223549d
Meng Bo <mengbo@lnu.edu.cn>
2017-02-14 14:00:42 +0800

    Use pkg file.

--- Casks/wch-ch34x-usb-serial-driver.rb
+++ Casks/wch-ch34x-usb-serial-driver.rb
@@ -1,15 +1,12 @@
 cask 'wch-ch34x-usb-serial-driver' do
   version '1.3'
-  sha256 'bd506b1095fd3cc5fff292a8fb26629b8678dff795de2d8f8464c948beb9d516'
+  sha256 '33156fd45a10efd50bede069c2081687d65053c88320a7166e0e05841c8f9c5b'
 
-  url 'https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver/raw/master/CH34x_Install_V1.3.zip'
+  url 'https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver/raw/master/CH34x_Install_V1.3.pkg'
   name 'WCH USB serial driver for CH340/CH341 (unofficial release)'
   homepage 'https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver'
   license :gratis
 
-
-  container type:   :zip,
-            nested: 'CH34x_Install_V1.3.pkg'
 
   pkg 'CH34x_Install_V1.3.pkg'
 
