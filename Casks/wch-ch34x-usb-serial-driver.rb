f73c18e506fd6412542d202f8ce15319280f1e0a
Thomas Jürges <yfn+gitlab@protonmail.com>
2019-04-14 13:34:03 +0200

    Update to driver version 1.5 for hombrew cask

--- Casks/wch-ch34x-usb-serial-driver.rb
+++ Casks/wch-ch34x-usb-serial-driver.rb
@@ -1,14 +1,14 @@
 cask 'wch-ch34x-usb-serial-driver' do
-  version '1.4'
-  sha256 '032e85afc2dfeaba6e77637718749e3af381892305c2c02c12d1cd2bb3585bc3'
+  version '1.5'
+  sha256 '21d28f3ea0a5625d13ace61f7da715704bffce28fb57a0924b9443b8d3ed6139'
 
-  url 'https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver/raw/master/CH34x_Install_V1.4.pkg'
+  url 'https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver/raw/master/CH34x_Install_V1.5.pkg'
   name 'WCH USB serial driver for CH340/CH341 (unofficial release)'
   homepage 'https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver'
 
   license :gratis
 
-  pkg 'CH34x_Install_V1.4.pkg'
+  pkg 'CH34x_Install_V1.5.pkg'
 
   uninstall pkgutil: 'com.wch.usbserial',
             kext:    'com.wch.usbserial',
